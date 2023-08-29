7168ca9d57e3b3d9473cefacbaf6859ab466b741
Lucas Tymen <lucas.tymen@gmail.com>
2023-08-29 18:00:17 +0200

    setting up templates

--- inventory/models.py
+++ inventory/models.py
@@ -2,10 +2,23 @@ from django.db import models
 
 # Create your models here.
 class Ingredient(models.Model):
-    ingredient_name = models.CharField(max_length="40"),
-    available_quantity = models.IntegerField(default=0),
-    prices_per_unit = models.FloatField(default=0),
+    name = models.CharField(max_length=40, unique=True)
+    quantity = models.FloatField(default=0)
+    unit = models.CharField(max_length=10)
+    prices_per_unit = models.FloatField(default=0)
+
 
 class MenuItem(models.Model):
-    menu_item = models.CharField(max_length="40"),
-    price_set = models.FloatField(default=0),
+    title = models.CharField(max_length=40)
+    price_menu = models.FloatField(default=0)
+
+
+class RecipeRequirement(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.FloatField(default=0)
+
+
+class Purchase(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
